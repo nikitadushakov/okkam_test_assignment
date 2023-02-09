ad089a92f5241346a53f791c73ba6830a80ef290
Nikita Dushakov <sir555nd@mail.ru>
2023-02-09 10:39:03 +0300

    add interface

---
+++ api/static/main.js
@@ -0,0 +1,15 @@
+let a1_input = document.querySelector(".au1 input");
+let a2_input = document.querySelector(".au2 input");
+// console.dir(a1_input);
+let button = document.querySelector(".button");
+let result = document.querySelector(".result");
+
+const get_percent = async (audience_1, audience_2) => {
+    let res = await fetch(`${window.origin}/getPercent?audience1=${audience_1}&audience2=${audience_2}`);
+    let json = await res.json()
+    return json['percent']
+}
+button.addEventListener("click", async (e) => {
+    let res = await get_percent(a1_input.value, a2_input.value); 
+    result.innerHTML = res;
+});
